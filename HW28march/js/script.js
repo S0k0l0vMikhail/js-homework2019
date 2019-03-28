aab59c76fbfaac973238910af4cb58138dc0ee11
sokolov <dolgopeh@gmail.com>
2019-03-28 16:21:55 +0300

    create hw

---
+++ HW28march/js/script.js
@@ -0,0 +1,62 @@
+// первое задание.
+console.log('первое задание');
+
+let height = 15;
+let length = 25;
+let width = 20;
+// S = 2(ab + bc + ac)
+
+let sp;
+
+sp = 2*(height*length + length*width + height*width);
+console.log(sp); //2350
+console.log(Math.max(height, width)); //20
+
+var res = (height > width) ? console.log(height): console.log(width);
+
+
+// второе задание
+console.log('второе задание');
+
+let garden = 1000;
+let g1 = 15;
+let g2 = 25;
+
+let sg;
+
+let g12 = 15  * 25;
+
+sg = garden % g12;
+console.log (sg); //250
+
+// третье задание
+console.log('третье задание');
+
+let bsd = 15;
+let lsm = 600;
+
+let bsm = 15 *100;
+
+//S=π  (R2−r2)
+
+console.log (Math.PI*(bsm-bsd));
+
+// четвертое задание
+console.log('четвертое задание');
+
+let X = 20;
+let Y = 10;
+
+if (X>=Y) {
+	console.log("новое значене: ", X, Y);
+} else {
+	console.log("значения не менялись: ", X, Y);
+}
+// пятое задание
+console.log('пятое задание');
+let m = 9;
+let n = 9.5;
+let z = 10;
+let zm = 10 - m;
+let zn = 10 - n;
+res = (zn === zm) ? console.log("значения равны") : (zn > zm) ? console.log(m):console.log(n) ;
