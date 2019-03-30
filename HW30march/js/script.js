2c61a7f522152c47eb0d4bc003a3807fb775b878
sokolov <dolgopeh@gmail.com>
2019-03-30 17:49:40 +0300

    upd

--- HW30march/js/script.js
+++ HW30march/js/script.js
@@ -23,10 +23,11 @@ console.log('второе задание');
 
 for (var x = 1; x <101; x++ ){
 
-  var y = (x % 3) + (x % 5);
+  //var y = (x % 3) + (x % 5);
   var t = (x % 3);
   var f = (x % 5);
-  if (y!=0 && t!=0 && f!=0) {
+  //if (y!=0 && t!=0 && f!=0) {
+  if (t!=0 && f!=0) {
     console.log(x);
   } else {
     if (t === 0 && f === 0) {console.log("ThreeFive");}
