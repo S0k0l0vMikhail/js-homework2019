6c46ce39f456f59377d2aca62f43bcb5858c09d8
sokolov <dolgopeh@gmail.com>
2019-04-02 15:47:16 +0300

    add

---
+++ HW02aprl/js/script.js
@@ -0,0 +1,64 @@
+(function () {
+  "use strict";
+
+console.log("1 task");
+  var arr1 = [1,2,3,8], i;
+  //for (i = 0; i < 10; i++ ) {
+  //arr1.push( Math.round( Math.random() * 10 ));}
+
+  var arr2 = [1,2,3,4];
+  //for (i = 0; i < 10; i++ ) {
+  //arr2.push( Math.round( Math.random() * 10 ));}
+  //arr2 = arr1.slice();
+
+  console.log (arr1, arr2);
+
+  function compare(arrname1, arrname2) {
+    if (!Array.isArray(arrname1 && arrname2)){
+      console.log("не массив");
+      return;
+    } else if (!(arrname1.length === arrname2.length)) {
+      console.log("не равны т.к. длина разная");
+      return;
+    } else {
+      for (var i = 0; i<arr1.length; i++){
+        if (!(arr1[i]===arr2[i])){console.log("не равны");return;}
+      }
+      console.log("РАВНЫ!");
+      return;
+    }
+  }
+  compare(arr1 , arr2);
+
+console.log("2 task");
+
+  /*function recursion(num) {
+      var nut = num.slice();
+      console.log(nut);
+      return;
+  }
+  recursion(234);*/
+
+console.log("3 task");
+
+  function range(num1, num2, num3) {
+      if (!(num1 && num2)) {
+          console.log("нет обязательных параметров");
+      }
+      var arr = [];
+      num3 = num3 || 1;
+      for (var i = num1; i <= num2; i=i+num3) {
+        arr.push(i);
+      }
+      console.log(arr);
+      return;
+  }
+
+  range(1,10,2);
+}())
+
+console.log("3 task");
+
+  //function functionName(count) {
+
+  //}
